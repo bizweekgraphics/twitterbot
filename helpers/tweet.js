205e560d7196528b74306952e74018014501d28d
dyep49 <dyep49@gmail.com>
2014-11-10 16:25:52 -0500

    changes api key for env variables

--- helpers/tweet.js
+++ helpers/tweet.js
@@ -2,10 +2,10 @@ var Twit = require('twit')
 
 var Twitter = function() {
   this.Bot = new Twit({
-    consumer_key: 'FPishWzIVPKZn8LAp7oBXjUl0'
-  , consumer_secret: 'qBP0rt7B7V8iteQ3B3MvA73vPiA3hcFCYup2q4cBTuKWAcbahT'
-  , access_token: '2499044862-9VpmtdYzNair4dRSGXrtV25U19f6OGZIW446OP4'
-  , access_token_secret: 'Zedo7zADU9HpOy3qeSB6jIhSwttJcHUtHqSWOOCs9QrBK'
+    consumer_key: process.env.FCCLIEFS_API_KEY
+  , consumer_secret: process.env.FCCLIEFS_API_SECRET_KEY
+  , access_token: process.env.FCCLIEFS_ACCESS_TOKEN
+  , access_token_secret: process.env.FCCLIEFS_TOKEN_SECRET
   })
 
   this.tweetSearch = function(query, callback) {
